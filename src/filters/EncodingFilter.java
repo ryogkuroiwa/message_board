3484053584964a96bebf44b411e236af49a37b79
ryouga.kuroiwa2 <kr525jp@gmail.com>
2023-11-05 16:10:59 +0900

    フラッシュメッセージを追加

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -20,24 +20,23 @@ public class EncodingFilter implements Filter {
      * Default constructor.
      */
     public EncodingFilter() {
-        // TODO Auto-generated constructor stub
+
     }
 
     /**
      * @see Filter#destroy()
      */
     public void destroy() {
-        // TODO Auto-generated method stub
+
     }
 
     /**
      * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
      */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        // place your code here
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 
-        // pass the request along the filter chain
         chain.doFilter(request, response);
     }
 
@@ -45,7 +44,6 @@ public class EncodingFilter implements Filter {
      * @see Filter#init(FilterConfig)
      */
     public void init(FilterConfig fConfig) throws ServletException {
-        // TODO Auto-generated method stub
-    }
 
+    }
 }
